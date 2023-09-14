d1a59f67dc99bfe73ad4babbad084ef01523c1b0
Kent <75526853+totallykent@users.noreply.github.com>
2023-09-14 16:14:56 +0800

    Big changes I guess

---
+++ Activity.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Activity</title>
+</head>
+<body>
+    <?php
+        $name = "Bob";
+
+        echo "Hello, my name is ".$name."!";
+    ?>
+</body>
+</html>
